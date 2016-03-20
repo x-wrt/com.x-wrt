c035d2a69ba1736014248fc48276472c820e012b
Chen Minqiang <ptpt52@gmail.com>
2016-03-20 16:56:48 +0800

    ipset support for gfwlist

--- natcap/files/gfwlist_update.sh
+++ natcap/files/gfwlist_update.sh
@@ -1,15 +1,33 @@
 #!/bin/sh
 
+EX_DOMAIN="google.com \
+		   google.com.hk \
+		   google.com.tw \
+		   google.com.sg \
+		   google.co.jp \
+		   blogspot.com \
+		   blogspot.sg \
+		   blogspot.hk \
+		   blogspot.jp \
+		   gvt1.com \
+		   gvt2.com \
+		   gvt3.com \
+		   1e100.net \
+		   blogspot.tw"
+
+ipset create gfwlist iphash
+ipset flush gfwlist
+
 rm -f /tmp/gfwlist.txt
 rm -f /tmp/accelerated-domains.gfwlist.dnsmasq.conf
-/usr/bin/wget --no-check-certificate -qO /tmp/gfwlist.txt "https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt"
-cat /tmp/gfwlist.txt | base64 -d | grep -v ^! | grep -o '[a-zA-Z0-9][-a-zA-Z0-9]*[.][^.][-a-zA-Z0-9.]*[a-zA-Z]$' | sort | uniq | while read line; do
-	echo server=/$line/8.8.8.8 >>/tmp/accelerated-domains.gfwlist.dnsmasq.conf
-done
-rm -f /tmp/gfwlist.txt
-
-num=`cat /tmp/accelerated-domains.gfwlist.dnsmasq.conf | wc -l`
-[ x$num != x0 ] && {
+/usr/bin/wget --no-check-certificate -qO /tmp/gfwlist.txt "https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt" && {
+	for w in `echo $EX_DOMAIN`" "`cat /tmp/gfwlist.txt | base64 -d | grep -v ^! | grep -o '[a-zA-Z0-9][-a-zA-Z0-9]*[.][-a-zA-Z0-9.]*[a-zA-Z]$'`; do
+		echo $w
+	done | sort | uniq | while read line; do
+		echo server=/$line/8.8.8.8 >>/tmp/accelerated-domains.gfwlist.dnsmasq.conf
+		echo ipset=/$line/gfwlist >>/tmp/accelerated-domains.gfwlist.dnsmasq.conf
+	done
+	rm -f /tmp/gfwlist.txt
 	mkdir -p /tmp/dnsmasq.d && mv /tmp/accelerated-domains.gfwlist.dnsmasq.conf /tmp/dnsmasq.d/accelerated-domains.gfwlist.dnsmasq.conf && /etc/init.d/dnsmasq restart
 }
 
