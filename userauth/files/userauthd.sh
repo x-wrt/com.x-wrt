9b9bf1d3cccdcb25c499ee73fae180e26cc666a2
Chen Minqiang <ptpt52@gmail.com>
2016-05-08 21:59:22 +0800

    bug fix

--- userauth/files/userauthd.sh
+++ userauth/files/userauthd.sh
@@ -19,7 +19,7 @@ nginx_server_conf=""
 
 for i in `seq 0 255`; do
 	ipset -n list auth_access_list$i >/dev/null 2>&1 || break
-	port=$((8000+i))
+	port=$((8001+i))
 	auth=login-$i
 	nginx_server_conf="$nginx_server_conf$(echo "$nginx_server_conf_tpl" | sed "s/_PORT_/$port/;s/_AUTH_/$auth/")"
 done
@@ -27,10 +27,11 @@ done
 rm -f /tmp/nginx.conf.tmp
 cat /usr/share/userauth/nginx.conf >>/tmp/nginx.conf.tmp
 echo "$nginx_server_conf" >>/tmp/nginx.conf.tmp
+echo "}" >>/tmp/nginx.conf.tmp
 mv /tmp/nginx.conf.tmp /tmp/nginx.conf
 /etc/init.d/nginx reload
 
-mkdir /tmp/userauth && {
+mkdir /tmp/userauth >/dev/null 2>&1 && {
 	test -e /tmp/userauth/www || ln -s /usr/share/userauth/www /tmp/userauth/www
 	test -e /tmp/userauth/cgi-bin || ln -s /usr/share/userauth/cgi-bin /tmp/userauth/cgi-bin
 }
