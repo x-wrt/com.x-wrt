3878165bedaf406c97dbd14fa403eeefe939102e
Chen Minqiang <ptpt52@gmail.com>
2016-05-10 10:39:46 +0800

    use lua-nginx refine userauth

--- userauth/files/userauthd.sh
+++ userauth/files/userauthd.sh
@@ -12,16 +12,23 @@
 nginx_server_conf_tpl="
     server {
         listen _PORT_;
-        rewrite ^(.*) http://10.1.0.8/_AUTH_/\$scheme://\$host\$request_uri redirect;
+        rewrite_by_lua '
+            ngx.header.cache_control = \"private, no-cache\";
+            local url = require(\"socket.url\");
+            local req_url = url.escape(ngx.var.scheme .. \"://\" .. ngx.var.host .. ngx.var.request_uri);
+            return ngx.redirect(\"http://_SERVER_/login.lua?aid=_AID_&ts=\" .. ngx.time() .. \"&url=\" .. req_url);
+        ';
     }
 "
 nginx_server_conf=""
 
-for i in `seq 0 255`; do
-	ipset -n list auth_online_list$i >/dev/null 2>&1 || break
-	port=$((8001+i))
-	auth=login-$i
-	nginx_server_conf="$nginx_server_conf$(echo "$nginx_server_conf_tpl" | sed "s/_PORT_/$port/;s/_AUTH_/$auth/")"
+for rule_index in `seq 0 255`; do
+	ipset -n list auth_online_list$rule_index >/dev/null 2>&1 || break
+	server_ip=`uci get userauth.@rule[$rule_index].server_ip`
+	test -n "$server_ip" || server_ip=10.$((0+rule_index)).0.8
+	port=$((8001+rule_index))
+	aid=$rule_index
+	nginx_server_conf="$nginx_server_conf$(echo "$nginx_server_conf_tpl" | sed "s/_PORT_/$port/;s/_SERVER_/$server_ip/;s/_AID_/$aid/")"
 done
 
 rm -f /tmp/nginx.conf.tmp
