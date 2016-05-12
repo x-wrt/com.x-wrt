f7100fbac978b3258b8d67cea832684d026700f6
Chen Minqiang <ptpt52@gmail.com>
2016-05-13 07:51:39 +0800

    stage2

--- userauth/files/lua/ngx-auth-redirect.lua
+++ userauth/files/lua/ngx-auth-redirect.lua
@@ -1,19 +1,7 @@
 if ngx.req.get_method() ~= 'GET' then
 	ngx.exit(ngx.HTTP_FORBIDDEN)
 end
-local st = nil
-if ngx.var.host == 'captive.apple.com' then
-	local rs = ngx.shared.auth_status
-	local key = 'st:' .. ngx.var.remote_addr
-	if rs:get(key) == 1 then
-		rs:set(key, 0)
-		st = 0
-	else
-		rs:set(key, 1)
-		st = 1
-	end
-end
-if not st or st ~= 0 then
+if not request_need_proxypass(ngx) then
 	local args = ngx.encode_args({
 			aid = ngx.var.aid,
 			url = ngx.var.req_url,
