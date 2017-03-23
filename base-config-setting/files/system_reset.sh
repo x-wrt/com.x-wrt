876b77705d7b70d91d0b5152dfd3fd86261a5d03
Chen Minqiang <ptpt52@gmail.com>
2017-03-23 11:31:39 +0800

    update x86 setting

---
+++ base-config-setting/files/system_reset.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+[ x"$1" = x"-f" -o x"$1" = x"-h" ] || {
+	echo "usage: $0 -f/-h"
+	exit 0
+}
+
+if test -f /rom/lib/preinit/79_disk_ready; then
+	sleep 1
+	mount -o remount,rw /rom && rm -f /rom/etc/sda3.ready
+else
+	sleep 1
+	killall dropbear uhttpd
+	test -f /etc/init.d/nginx && /etc/init.d/nginx stop
+	test -f /etc/init.d/database && /etc/init.d/database stop
+	sleep 1
+	jffs2reset -y
+fi
+
+[ x"$1" = x"-h" ] && halt && exit 0
+[ x"$1" = x"-f" ] && reboot && exit 0
+
+exit 0
+
+# 1. to reset config: reset /overlay: remove /rom/etc/sda3.ready and rebboot
+# 2. to reset /data: echo erase >/dev/sda4 and reboot
