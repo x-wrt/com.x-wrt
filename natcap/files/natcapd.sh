96a60493faf96929748d09255eae83e9aac47c0e
Chen Minqiang <ptpt52@gmail.com>
2018-05-04 11:47:45 +0800

    natcap_touch_timeout option

--- natcap/files/natcapd.sh
+++ natcap/files/natcapd.sh
@@ -241,6 +241,7 @@ uci get firewall.natcapd >/dev/null 2>&1 || {
 	clear_dst_on_reload=`uci get natcapd.default.clear_dst_on_reload 2>/dev/null || echo 0`
 	server_persist_timeout=`uci get natcapd.default.server_persist_timeout 2>/dev/null || echo 30`
 	tx_speed_limit=`uci get natcapd.default.tx_speed_limit 2>/dev/null || echo 0`
+	touch_timeout=`uci get natcapd.default.touch_timeout 2>/dev/null || echo 32`
 	servers=`uci get natcapd.default.server 2>/dev/null`
 	dns_server=`uci get natcapd.default.dns_server 2>/dev/null`
 	knocklist=`uci get natcapd.default.knocklist 2>/dev/null`
@@ -296,6 +297,7 @@ uci get firewall.natcapd >/dev/null 2>&1 || {
 	echo clean >>$DEV
 	echo server_persist_timeout=$server_persist_timeout >>$DEV
 	echo tx_speed_limit=$tx_speed_limit >>$DEV
+	echo natcap_touch_timeout=$touch_timeout >>$DEV
 	echo encode_mode=$encode_mode >$DEV
 	echo udp_encode_mode=$udp_encode_mode >$DEV
 	echo sproxy=$sproxy >$DEV
