7de0f414c1dca0b99e53859d5c1761345c46b87d
Chen Minqiang <ptpt52@gmail.com>
2019-08-08 22:20:47 +0800

    add hc5671a

--- rom/lede/makemenuconfig_parse.sh
+++ rom/lede/makemenuconfig_parse.sh
@@ -375,6 +375,7 @@ for t in $targets; do
 		TARGET_DEVICE_kirkwood_DEVICE_on100|\
 		TARGET_DEVICE_kirkwood_DEVICE_linksys_audi|\
 		TARGET_DEVICE_ramips_mt76x8_DEVICE_hiwifi_hc5661a|\
+		TARGET_DEVICE_ramips_mt76x8_DEVICE_hiwifi_hc5761a|\
 		TARGET_DEVICE_ar71xx_generic_DEVICE_F9K1115V2|\
 		TARGET_DEVICE_ar71xx_tiny_DEVICE_F9K1115V2|\
 		TARGET_DEVICE_ipq806x_DEVICE_qcom_ap-dk04.1-c1|\
@@ -780,6 +781,7 @@ for t in $targets; do
 		TARGET_DEVICE_ar71xx_generic_DEVICE_tl-wdr7500-v3|\
 		TARGET_DEVICE_ar71xx_generic_DEVICE_archer-c7-v4|\
 		TARGET_DEVICE_ramips_mt76x8_DEVICE_hiwifi_hc5661a|\
+		TARGET_DEVICE_ramips_mt76x8_DEVICE_hiwifi_hc5761a|\
 		TARGET_DEVICE_brcm47xx_generic_DEVICE_linksys-wrt610n-v2|\
 		TARGET_DEVICE_brcm47xx_generic_DEVICE_linksys-wrt610n-v1|\
 		TARGET_DEVICE_ar71xx_generic_DEVICE_DGL5500A1|\
