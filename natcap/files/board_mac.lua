6244f527ea434171f8ae3749ad808ee74308cb61
Chen Minqiang <ptpt52@gmail.com>
2019-10-01 00:21:35 +0800

    natcap: if has label_macaddr get it

--- natcap/files/board_mac.lua
+++ natcap/files/board_mac.lua
@@ -8,6 +8,11 @@ f:close()
 
 t = js.decode(t)
 
+if t.system and t.system.label_macaddr then
+	print(string.upper(t.system.label_macaddr))
+	os.exit(0)
+fi
+
 if t.network and t.network.lan and t.network.lan.macaddr then
 	print(string.upper(t.network.lan.macaddr))
 	os.exit(0)
