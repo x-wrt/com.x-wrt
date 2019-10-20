dec4fe6d97ce63ebe6fc70bb8550e2bce4e60107
Chen Minqiang <ptpt52@gmail.com>
2019-10-21 01:29:56 +0800

    natcap: openvpn cipher AES-256-CBC auth SHA256

--- natcap/files/natcapd.openvpn.sh
+++ natcap/files/natcapd.openvpn.sh
@@ -104,6 +104,8 @@ make_config()
 			uci add_list openvpn.natcapovpn_$p.push='dhcp-option DNS 8.8.8.8'
 			uci set openvpn.natcapovpn_$p.proto="${p}4"
 			uci set openvpn.natcapovpn_$p.verb='3'
+			uci set openvpn.natcapovpn_$p.cipher='AES-256-CBC'
+			uci set openvpn.natcapovpn_$p.auth='SHA256'
 			I=$((I+1))
 		done
 		uci commit openvpn
