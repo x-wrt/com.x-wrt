33c8c954e509781dfad8f71fc63cb8c0f0915af6
Chen Minqiang <ptpt52@gmail.com>
2019-11-12 07:39:05 +0800

    auto get TAG from .build_x/env

--- rom/lede/tag-backup.sh
+++ rom/lede/tag-backup.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
-TAG=${TAG-x-b`date +%Y%m%d%H%M`}
+fail()
+{
+	echo $@
+	exit 255
+}
+
+test -f .build_x/env && \
+. .build_x/env && \
+TAG=${TAG-$CONFIG_VERSION_NUMBER} &&
+test -n "$TAG" || fail no TAG
 
 sed -i "s/\(^src-git.*\.git$\)/\1;$TAG/" feeds.conf.default && \
 git commit --signoff -am "release: $TAG" && \
 git tag $TAG && \
-git push origin $TAG
+git push origin $TAG || exit 1
 
 cd feeds/x && \
 sed -i "s/CONFIG_VERSION_NUMBER=\".*\"/CONFIG_VERSION_NUMBER=\"$TAG\"/" rom/lede/config.* && \
@@ -13,16 +22,14 @@ git commit --signoff -am "release: $TAG" && \
 git tag $TAG && \
 git push origin $TAG && \
 git push origin master && \
-cd -
-
-exit 0
+cd - || exit 1
 
 for d in feeds/packages feeds/luci feeds/routing feeds/telephony; do
 	cd "$d" && {
 		echo
 		pwd
 		git tag $TAG && \
-		git push origin $TAG
+		git push origin $TAG || exit 1
 		cd -
-	}
+	} || exit 1
 done
