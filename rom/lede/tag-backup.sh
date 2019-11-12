8ff770ea0072af81a7d0a879d25551e8f379e6af
Chen Minqiang <ptpt52@gmail.com>
2019-11-12 08:13:43 +0800

    set TAG as version if release

--- rom/lede/tag-backup.sh
+++ rom/lede/tag-backup.sh
@@ -7,22 +7,31 @@ fail()
 }
 
 test -f .build_x/env && \
-. .build_x/env && \
+. .build_x/env
+
+test -n "$TAG" && release=0
+
 TAG=${TAG-$CONFIG_VERSION_NUMBER} &&
 test -n "$TAG" || fail no TAG
 
-sed -i "s/\(^src-git.*\.git$\)/\1;$TAG/" feeds.conf.default && \
-git commit --signoff -am "release: $TAG" && \
+[ "x$release" = "x0" ] || {
+	sed -i "s/\(^src-git.*\.git$\)/\1;$TAG/" feeds.conf.default && \
+	git commit --signoff -am "release: $TAG"
+}
+
 git tag $TAG && \
 git push origin $TAG || exit 1
 
-cd feeds/x && \
-sed -i "s/CONFIG_VERSION_NUMBER=\".*\"/CONFIG_VERSION_NUMBER=\"$TAG\"/" rom/lede/config.* && \
-git commit --signoff -am "release: $TAG" && \
+cd feeds/x
+[ "x$release" = "x0" ] || {
+	sed -i "s/CONFIG_VERSION_NUMBER=\".*\"/CONFIG_VERSION_NUMBER=\"$TAG\"/" rom/lede/config.* && \
+	git commit --signoff -am "release: $TAG" && \
+	git push origin master
+}
+
 git tag $TAG && \
-git push origin $TAG && \
-git push origin master && \
-cd - || exit 1
+git push origin $TAG || exit 1
+cd -
 
 for d in feeds/packages feeds/luci feeds/routing feeds/telephony; do
 	cd "$d" && {
