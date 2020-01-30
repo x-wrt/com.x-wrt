51b9f7b7739faa2750e5819581cdf1b7c9d8eb42
Chen Minqiang <ptpt52@gmail.com>
2020-01-30 13:49:43 +0800

    match ipset dst ui

--- luci-app-natcap/files/luci/model/cbi/natcap/natcap_route.lua
+++ luci-app-natcap/files/luci/model/cbi/natcap/natcap_route.lua
@@ -10,8 +10,33 @@ local s = m:section(TypedSection, "natcapd", "")
 s.addremove = false
 s.anonymous = true
 
+--rulesets
+local u = m:section(TypedSection, "ruleset", "")
+u.addremove = true
+u.anonymous = true
+u.template = "cbi/tblsection"
+
+e = u:option(Value, "src", translate("From Client(s)"))
+e.datatype = "string"
+e.rmempty  = false
+e.placeholder = "192.168.1.100 or AA:00:11:23:44:55"
+
+e = u:option(Value, "dst", translate("To Destination"))
+e.datatype = "string"
+e.rmempty  = false
+e.placeholder = "ipset name"
+
+e = u:option(ListValue, "target", translate("Target Gateway"))
+e:value("", translate("Please select..."))
+local ut = require "luci.util"
+local sys  = require "luci.sys"
+local text = ut.trim(sys.exec("cat /dev/natcap_ctl"))
+for ip in text:gmatch("server ([0-9.]+[^\n]+)") do
+	e:value(ip)
+end
+
 --rules
-local u = m:section(TypedSection, "rule", "")
+u = m:section(TypedSection, "rule", "")
 u.addremove = true
 u.anonymous = true
 u.template = "cbi/tblsection"
