8a11463f2efef9b3856cbfef97cab9a614657a55
Chen Minqiang <ptpt52@gmail.com>
2020-05-25 19:38:45 +0800

    natcap: update json format

--- natcap/files/yy.sn.json.lua
+++ natcap/files/yy.sn.json.lua
@@ -10,8 +10,8 @@ t = js.decode(t)
 
 if t.code == 0 then
 	print("Success!");
-	if t.exp then
-		print("Expire Date: %s" % os.date('%Y-%m-%d %H:%M:%S', t.exp))
+	if t.data and t.data.exp then
+		print("Expire Date: %s" % os.date('%Y-%m-%d %H:%M:%S', t.data.exp))
 	end
 else
 	print("Fail!");
