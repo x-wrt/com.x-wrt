b6f03015917cf7c88be3fe8abfec4c7297601a16
Chen Minqiang <ptpt52@gmail.com>
2020-06-06 05:32:03 +0800

    filter out dist by config in .build_x

--- rom/lede/gen_map.sh
+++ rom/lede/gen_map.sh
@@ -20,7 +20,7 @@ echo sha256sums: sha256sums.txt >>map.list
 echo get x86bin
 x86bin="`find bin/targets/ | grep -- '\(-combined\|-uefi\|combined-efi\)' | sort | while read line; do basename $line; done`"
 x86bin=`for cfg in $CFGS; do
-	cat feeds/x/rom/lede/$cfg | grep CONFIG_VERSION_DIST | sed 's/"//g' | cut -d= -f2 | tr A-Z a-z | while read dist; do
+	cat .build_x/$cfg | grep CONFIG_VERSION_DIST | sed 's/"//g' | cut -d= -f2 | tr A-Z a-z | while read dist; do
 		for bin in $x86bin; do
 			echo $bin | grep "$dist-"
 		done
