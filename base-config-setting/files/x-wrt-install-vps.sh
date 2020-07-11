5297358c6fed199b60a10e313d99608af849d273
Chen Minqiang <ptpt52@gmail.com>
2020-07-12 07:42:12 +0800

    update install vps

--- base-config-setting/files/x-wrt-install-vps.sh
+++ base-config-setting/files/x-wrt-install-vps.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-vmroot=$(dirname "$0")
+vmroot=/tmp/block
+mkdir -p $vmroot
+mount /dev/sda1 $vmroot || exit 0
 cp $vmroot/x-wrt.img.gz /tmp/x-wrt.img.gz && {
+	cd /
+	umount $vmroot
+	sync
 	(zcat /tmp/x-wrt.img.gz;
 	 echo network=172.21.170.245,255.255.240.0,172.21.175.253,8.8.8.8;
-	) >/dev/vda && reboot
+	) >/dev/sda && reboot
 }
