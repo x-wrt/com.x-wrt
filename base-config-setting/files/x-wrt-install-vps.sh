36f944c630762873e8d2abf11bd1ef8fcc7160cf
Chen Minqiang <ptpt52@gmail.com>
2020-07-12 09:30:09 +0800

    refine sample x-wrt-install-vps.sh

--- base-config-setting/files/x-wrt-install-vps.sh
+++ base-config-setting/files/x-wrt-install-vps.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+BDEV=sda
+#network=172.21.170.245,255.255.240.0,172.21.175.253,8.8.8.8
+network=dhcp
+
 vmroot=/tmp/block
 mkdir -p $vmroot
-mount /dev/sda1 $vmroot || exit 0
+mount /dev/${BDEV}1 $vmroot || exit 0
 cp $vmroot/x-wrt.img.gz /tmp/x-wrt.img.gz && {
 	cd /
 	umount $vmroot
 	sync
 	(zcat /tmp/x-wrt.img.gz;
-	 echo network=172.21.170.245,255.255.240.0,172.21.175.253,8.8.8.8;
-	) >/dev/sda && reboot
+	 echo network=$network;
+	) >/dev/$BDEV && reboot
 }
