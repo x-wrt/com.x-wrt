5da058e624278db03f74f933de082c0c50db3777
Chen Minqiang <ptpt52@gmail.com>
2020-11-12 02:14:56 +0800

    init mwan3 from https://github.com/x-wrt/packages/tree/mwan3-x-wrt-base

---
+++ net/mwan3/files/lib/mwan3/common.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+get_uptime() {
+	local uptime=$(cat /proc/uptime)
+	echo "${uptime%%.*}"
+}
+
+SCRIPTNAME="$(basename "$0")"
+MWAN3TRACK_STATUS_DIR="/var/run/mwan3track"
+LOG()
+{
+	local facility=$1; shift
+	# in development, we want to show 'debug' level logs
+	# when this release is out of beta, the comment in the line below
+	# should be removed
+	[ "$facility" = "debug" ] && return
+	logger -t "${SCRIPTNAME}[$$]" -p $facility "$*"
+}
