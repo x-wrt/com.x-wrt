7c7856dd4e5fefa12f23e2f4ec495542ce97eeb0
Chen Minqiang <ptpt52@gmail.com>
2020-11-15 15:48:36 +0800

    wifista: display status

--- luci-app-simplenetwork/htdocs/luci-static/resources/view/simplenetwork/wifista.js
+++ luci-app-simplenetwork/htdocs/luci-static/resources/view/simplenetwork/wifista.js
@@ -8,9 +8,13 @@
 'require form';
 'require network';
 
-var ssidValid;
+var callWwanStatus = rpc.declare({
+    object: 'network.interface.wwan',
+    method: 'status',
+    expect: { }
+});
 
-ssidValid = function(ssid) {
+var ssidValid = function(ssid) {
 	if (ssid.startsWith('None')) {
 		return 'none';
 	} else if (ssid.startsWith('WPA2 PSK')) {
@@ -27,8 +31,10 @@ ssidValid = function(ssid) {
 return view.extend({
 	load: function() {
 		return Promise.all([
+			uci.changes(),
 			uci.load('network'),
-			uci.load('wireless')
+			uci.load('wireless'),
+			L.resolveDefault(callWwanStatus(), [])
 		]);
 	},
 
@@ -44,6 +50,53 @@ return view.extend({
 		return m.render();
 	},
 
+	poll_status: function(map, data) {
+		var wwan_status = map.querySelector('[data-name="_wwan_status"]').querySelector('.cbi-value-field');
+		var status_ipaddr = map.querySelector('[data-name="_status_ipaddr"]').querySelector('.cbi-value-field');
+		var status_netmask = map.querySelector('[data-name="_status_netmask"]').querySelector('.cbi-value-field');
+		var status_gateway = map.querySelector('[data-name="_status_gateway"]').querySelector('.cbi-value-field');
+		var status_dns = map.querySelector('[data-name="_status_dns"]').querySelector('.cbi-value-field');
+
+		//console.log(data);
+		try {
+			if (data.length == 1) {
+				var ipaddr = data[0]['ipv4-address'][0]['address'];
+				var netmask = network.prefixToMask(+data[0]['ipv4-address'][0]['mask']);
+				var gateway = '0.0.0.0';
+				var dns = data[0]['dns-server'];
+				var routes = data[0]['route'];
+
+				for (var x = 0; x < routes.length; x++) {
+					if (routes[x].target == '0.0.0.0' && routes[x].mask == 0) {
+						gateway = routes[x].nexthop;
+						break;
+					}
+				}
+
+				status_ipaddr.innerHTML = ipaddr;
+				status_netmask.innerHTML = netmask;
+				status_gateway.innerHTML = gateway;
+				if (dns.length > 0) {
+					status_dns.innerHTML = dns[0];
+					for (var x = 1; x < dns.length; x++) {
+						status_dns.innerHTML = status_dns.innerHTML + " " + dns[x];
+					}
+				} else {
+					status_dns.innerHTML = '-';
+				}
+
+				wwan_status.innerHTML = '<p style="color:green;"><b>' + _('Connected') + '</b></p>';
+			}
+		} catch(err) {
+			status_ipaddr.innerHTML = '-';
+			status_netmask.innerHTML = '-';
+			status_gateway.innerHTML = '-';
+			status_dns.innerHTML = '-';
+
+			wwan_status.innerHTML = '<p style="color:red;"><b>' + _('Not Connected') + '</b></p>';
+		}
+	},
+
 	render: function(data) {
 		var m, s, ss, o;
 		var _this = this;
@@ -51,6 +104,7 @@ return view.extend({
 
 		m = new form.Map('wireless', [_('Wireless STA')],
 			_('Configure the WiFi STA'));
+		m.chain('network');
 
 		s = m.section(form.NamedSection, 'wifinet1', 'wifi-iface');
 		s.addremove = false;
@@ -156,7 +210,10 @@ return view.extend({
 		o.rmempty = true;
 		o.password = true;
 
-		m.chain('network');
+		o = s.option(form.DummyValue, '_wwan_status', _('Status'));
+		o.modalonly = false;
+		o.default = _('Loading..');
+
 		o = s.option(form.SectionValue, '_interface', form.TypedSection, 'interface', _('STA Network'));
 		ss = o.subsection;
 		ss.uciconfig = 'network';
@@ -190,6 +247,34 @@ return view.extend({
 		o.datatype = 'ip4addr';
 		o.cast = 'string';
 
-		return m.render();
+		o = ss.option(form.DummyValue, '_status_ipaddr', _('IPv4 address'));
+		o.depends('proto', 'dhcp');
+		o.modalonly = false;
+		o.default = '-';
+
+		o = ss.option(form.DummyValue, '_status_netmask', _('IPv4 netmask'));
+		o.depends('proto', 'dhcp');
+		o.modalonly = false;
+		o.default = '-';
+
+		o = ss.option(form.DummyValue, '_status_gateway', _('IPv4 gateway'));
+		o.depends('proto', 'dhcp');
+		o.modalonly = false;
+		o.default = '-';
+
+		o = ss.option(form.DummyValue, '_status_dns', _('DNS'));
+		o.depends('proto', 'dhcp');
+		o.modalonly = false;
+		o.default = '-';
+
+		return m.render().then(L.bind(function(m, nodes) {
+			//this.poll_status(nodes, data[3]);
+			poll.add(L.bind(function() {
+				return Promise.all([
+					L.resolveDefault(callWwanStatus(), [])
+				]).then(L.bind(this.poll_status, this, nodes));
+			}, this), 5);
+			return nodes;
+		}, this, m));
 	}
 });
