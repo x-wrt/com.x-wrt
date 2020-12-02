6818560067c2666761b59b36ba77e0484349e61f
Chen Minqiang <ptpt52@gmail.com>
2020-12-02 19:03:27 +0800

    net/mwan3: use nonblock try lock

--- net/mwan3plus/files/lib/mwan3/mwan3.sh
+++ net/mwan3plus/files/lib/mwan3/mwan3.sh
@@ -266,8 +266,7 @@ mwan3_init()
 }
 
 mwan3_lock() {
-	while :; do pgrep -f "lock /var/run/mwan3.lock" >/dev/null 2>&1 || break; sleep 1; done
-	lock /var/run/mwan3.lock
+	while ! lock -n /var/run/mwan3.lock 2>/dev/null; do sleep 1; done
 	#LOG debug "$1 $2 (lock)"
 }
 
