d6b9082f19dc851a505213ab9a0201c53a8414d0
Chen Minqiang <ptpt52@gmail.com>
2021-01-04 01:33:28 +0800

    natcap: update version

--- natcap/files/natcapd.sh
+++ natcap/files/natcapd.sh
@@ -1105,8 +1105,8 @@ main_trigger() {
 	local hostip
 	local built_in_server
 	local crashlog=0
-	test -e /sys/kernel/debug/crashlog && crashlog=9
-	test -e /tmp/pstore && crashlog=9
+	test -e /sys/kernel/debug/crashlog && crashlog=10
+	test -e /tmp/pstore && crashlog=10
 	cp /usr/share/natcapd/cacert.pem /tmp/cacert.pem
 	while :; do
 		test -f $LOCKDIR/$PID || return 0
