cc63ad445578d0da95bcd64a337474cd09da0341
Chen Minqiang <ptpt52@gmail.com>
2021-09-29 01:14:09 +0800

    initramfs-upgrade-fw: use sysupgrade

--- initramfs-upgrade-fw/files/initramfs-upgrade-fw.sh
+++ initramfs-upgrade-fw/files/initramfs-upgrade-fw.sh
@@ -5,6 +5,9 @@ SERVER=192.168.1.254
 fw=http://$SERVER/factory_main.bin
 md5=http://$SERVER/factory_main.md5
 
+. /etc/diag.sh
+set_state upgrade
+
 cd /tmp
 
 while :; do
@@ -15,8 +18,9 @@ while :; do
 	wget --timeout=60 -O /tmp/x-wrt.bin $fw && {
 		wget --timeout=60 -O /tmp/x-wrt.bin.md5 $md5 && {
 			[ "$(md5sum /tmp/x-wrt.bin | head -c32)" = "$(cat /tmp/x-wrt.bin.md5 | head -c32)" ] && {
-				mtd write /tmp/x-wrt.bin firmware && {
-					fw_setenv sys_boot_flag 0
+				sysupgrade -n /tmp/x-wrt.bin || {
+					set_state failsafe
+					break
 				}
 			}
 		}
