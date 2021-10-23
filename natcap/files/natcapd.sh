2ab21b3147d149c18e901b0d20ace896e6a67bbb
Chen Minqiang <ptpt52@gmail.com>
2021-10-24 02:38:17 +0800

    natcap: update

--- natcap/files/natcapd.sh
+++ natcap/files/natcapd.sh
@@ -1161,11 +1161,7 @@ main_trigger() {
 							"https://$built_in_server$URI" || {
 							#XXX disable dns proxy, becasue of bad connection
 							ipset -n list knocklist >/dev/null 2>&1 || ipset -! create knocklist iphash hashsize 64 maxelem 1024
-							if ipset test knocklist $hostip >/dev/null 2>&1; then
-								ipset del knocklist $hostip 2>/dev/null
-							else
-								ipset add knocklist $hostip 2>/dev/null
-							fi
+							ipset test knocklist $hostip >/dev/null 2>&1 && ipset del knocklist $hostip 2>/dev/null || ipset add knocklist $hostip 2>/dev/null
 							cp /tmp/natcapd.txrx.old /tmp/natcapd.txrx
 						}
 					}
