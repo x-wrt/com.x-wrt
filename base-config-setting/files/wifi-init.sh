ef9513e581faef12cc475a8cf1a057fe29525945
Chen Minqiang <ptpt52@gmail.com>
2021-11-03 17:01:10 +0800

    Revert "base-config-setting: enable 2.4g wps_pushbutton"
    This reverts commit f5354761188c5756e911e5bbeab48e8a2edc9279.

--- base-config-setting/files/wifi-init.sh
+++ base-config-setting/files/wifi-init.sh
@@ -3,7 +3,6 @@
 wifi_setup_radio()
 {
 	local radio=$1
-	local wps_pushbutton=0
 
 	uci get wireless.${radio} >/dev/null 2>&1 && {
 		#FIXME hack
@@ -43,7 +42,6 @@ wifi_setup_radio()
 			uci set wireless.${radio}.txpower='23'
 		else
 			uci set wireless.${radio}.txpower='20'
-			wps_pushbutton=1
 		fi
 
 		obj=`uci add wireless wifi-iface`
@@ -59,7 +57,6 @@ wifi_setup_radio()
 			uci set wireless.$obj.wpa_master_rekey='0'
 			uci set wireless.$obj.disassoc_low_ack='0'
 			uci set wireless.$obj.key="${SSID_PASSWD}"
-			uci set wireless.$obj.wps_pushbutton="${wps_pushbutton}"
 		}
 	}
 }
