12130128ffb27c27da10a60ef77a67bad626186d
Chen Minqiang <ptpt52@gmail.com>
2022-03-24 21:47:24 +0800

    update config

--- rom/lede/makemenuconfig_parse.sh
+++ rom/lede/makemenuconfig_parse.sh
@@ -275,6 +275,7 @@ for t in $targets; do
 	excludes="$excludes_basic"
 	case $t in
 		#>8M flash
+		TARGET_DEVICE_ramips_mt7621_DEVICE_xwrt_t-cpe1201k-v01|\
 		TARGET_DEVICE_mediatek_mt7622_DEVICE_xwrt_wr3200k-v1|\
 		TARGET_DEVICE_mediatek_mt7622_DEVICE_mediatek_mt7622-rfb1-ubi|\
 		TARGET_DEVICE_mediatek_mt7622_DEVICE_mediatek_mt7622-rfb1|\
@@ -461,7 +462,6 @@ for t in $targets; do
 		TARGET_DEVICE_ath79_generic_DEVICE_letv_lba-047-ch|\
 		TARGET_DEVICE_ramips_mt7621_DEVICE_xwrt_t-cpe1202kd-v01|\
 		TARGET_DEVICE_ramips_mt7621_DEVICE_xwrt_t-cpe1200k-v01|\
-		TARGET_DEVICE_ramips_mt7621_DEVICE_xwrt_t-cpe1201k-v01|\
 		TARGET_DEVICE_ramips_mt7621_DEVICE_xwrt_x-sdwan-1200|\
 		TARGET_DEVICE_ramips_mt7621_DEVICE_xwrt_mac500f|\
 		TARGET_DEVICE_ramips_mt7621_DEVICE_xwrt_puppies|\
