194437539865ba0514cb7b5af32c595e03fd4bfe
Chen Minqiang <ptpt52@gmail.com>
2022-03-29 14:24:10 +0800

    config.ramips-mt7621: add planex_vr500

--- rom/lede/makemenuconfig_parse.sh
+++ rom/lede/makemenuconfig_parse.sh
@@ -275,6 +275,7 @@ for t in $targets; do
 	excludes="$excludes_basic"
 	case $t in
 		#>8M flash
+		TARGET_DEVICE_ramips_mt7621_DEVICE_planex_vr500|\
 		TARGET_DEVICE_ramips_mt7621_DEVICE_xwrt_t-cpe1201k-v01|\
 		TARGET_DEVICE_mediatek_mt7622_DEVICE_xwrt_wr3200k-v1|\
 		TARGET_DEVICE_mediatek_mt7622_DEVICE_mediatek_mt7622-rfb1-ubi|\
@@ -720,6 +721,7 @@ for t in $targets; do
 	#check usb
 	case $t in
 		#with usb3
+		TARGET_DEVICE_ramips_mt7621_DEVICE_planex_vr500|\
 		TARGET_DEVICE_mediatek_mt7622_DEVICE_xwrt_wr3200k-v1|\
 		TARGET_DEVICE_mediatek_mt7622_DEVICE_mediatek_mt7622-rfb1-ubi|\
 		TARGET_DEVICE_mediatek_mt7622_DEVICE_mediatek_mt7622-rfb1|\
