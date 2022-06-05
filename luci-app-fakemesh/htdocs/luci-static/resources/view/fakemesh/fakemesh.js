7178b35647204a3efafb6151caf22a422ddcee3d
Chen Minqiang <ptpt52@gmail.com>
2022-06-05 18:42:51 +0800

    luci-app-fakemesh: sync config: ui

--- luci-app-fakemesh/htdocs/luci-static/resources/view/fakemesh/fakemesh.js
+++ luci-app-fakemesh/htdocs/luci-static/resources/view/fakemesh/fakemesh.js
@@ -5,6 +5,7 @@
 'require uci';
 'require rpc';
 'require form';
+'require network';
 
 return view.extend({
 	load: function() {
@@ -59,6 +60,49 @@ return view.extend({
 		o.placeholder = '10.10.10.1'
 		o.rmempty = true;
 		o.datatype = 'ip4addr';
+
+		o = s.option(form.Flag, 'sync_ac', _('Sync Config'), _('Sync config from the Controller.'));
+		o.depends('role', 'wap');
+		o.depends('role', 'agent');
+		o.enabled = '1';
+		o.disabled = '0';
+		o.default = o.enabled;
+
+		s = m.section(form.GridSection, 'wifim', _('Wireless Management'));
+		s.addremove = true;
+		s.anonymous = true;
+		s.nodescriptions = true;
+		s.sortable = false;
+
+		o = s.option(form.Value, 'ssid', _('<abbr title="Extended Service Set Identifier">ESSID</abbr>'));
+		o.datatype = 'maxlength(32)';
+
+		o = s.option(form.ListValue, 'encryption', _('Encryption'));
+		o.value('none', _('No Encryption'));
+		o.value('psk', _('WPA-PSK'));
+		o.value('psk2', _('WPA2-PSK'));
+		o.value('psk-mixed', _('WPA-PSK/WPA2-PSK Mixed Mode'));
+		o.value('sae', _('WPA3-SAE'));
+		o.value('sae-mixed', _('WPA2-PSK/WPA3-SAE Mixed Mode'));
+
+		o = s.option(form.Value, 'key', _('Key'));
+		o.depends('encryption', 'psk');
+		o.depends('encryption', 'psk2');
+		o.depends('encryption', 'psk-mixed');
+		o.depends('encryption', 'sae');
+		o.depends('encryption', 'sae-mixed');
+		o.rmempty = false;
+		o.password = true;
+		o.datatype = 'wpakey';
+
+		o = s.option(form.ListValue, 'band', _('Band'));
+		o.value('2g5g', _('2G+5G'));
+		o.value('5g', _('5G'));
+		o.value('2g', _('2G'));
+		o.default = '2g5g';
+
+		o = s.option(form.Flag, 'enabled', _('Enable'));
+		o.default = o.enabled;
 
 		return m.render();
 	}
