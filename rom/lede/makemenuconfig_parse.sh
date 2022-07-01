030f59e28a8bf85cec55d432c6db01a1cc138eb3
Chen Minqiang <ptpt52@gmail.com>
2022-07-01 20:52:22 +0800

    add ac8000p

--- rom/lede/makemenuconfig_parse.sh
+++ rom/lede/makemenuconfig_parse.sh
@@ -466,6 +466,7 @@ for t in $targets; do
 		TARGET_DEVICE_ramips_mt7621_DEVICE_xwrt_t-cpe1201k-v01|\
 		TARGET_DEVICE_ramips_mt7621_DEVICE_xwrt_x-sdwan-1200|\
 		TARGET_DEVICE_ramips_mt7621_DEVICE_xwrt_mac500f|\
+		TARGET_DEVICE_ramips_mt7621_DEVICE_xwrt_ac8000p|\
 		TARGET_DEVICE_ramips_mt7621_DEVICE_xwrt_puppies|\
 		TARGET_DEVICE_ramips_mt7621_DEVICE_xwrt_nxc200p|\
 		TARGET_DEVICE_ramips_mt7621_DEVICE_xwrt_nxc2009e-v100|\
@@ -1040,6 +1041,7 @@ for t in $targets; do
 		TARGET_DEVICE_realtek_rtl838x_DEVICE_zyxel_gs1900-8hp-v2|\
 		TARGET_DEVICE_mediatek_mt7622_DEVICE_ruijie_rg-ew3200gx-pro|\
 		TARGET_DEVICE_ath79_generic_DEVICE_letv_lba-047-ch|\
+		TARGET_DEVICE_ramips_mt7621_DEVICE_xwrt_ac8000p|\
 		TARGET_DEVICE_ramips_mt7621_DEVICE_xwrt_puppies|\
 		TARGET_DEVICE_ramips_mt7621_DEVICE_xwrt_nxc200p|\
 		TARGET_DEVICE_ramips_mt7621_DEVICE_xwrt_nxc2009e-v100|\
