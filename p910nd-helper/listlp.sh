c66e76278b80d78825082fc5c8f25622cb15bd56
Chen Minqiang <ptpt52@gmail.com>
2022-08-07 07:58:27 +0800

    p910nd-helper: listlp

---
+++ p910nd-helper/listlp.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+usbmisc=/sys/class/usbmisc
+
+cd $usbmisc && {
+	for dev in $(ls); do
+		desc_file=$usbmisc/$dev/device/ieee1284_id
+		uevent_file=$usbmisc/$dev/device/uevent
+
+		test -e $usbmisc/$dev/device/ieee1284_id || continue
+
+		name=$(cat $desc_file | sed 's/.*DES:\(.*\);.*/\1/' | cut -d ';' -f 1)
+		model=$(cat $desc_file | sed 's/.*MDL:\(.*\);.*/\1/' | cut -d ';' -f 1)
+		product=`cat $uevent_file | grep PRODUCT= | sed 's/PRODUCT=\(.*\)/\1/'`
+
+		echo "/dev/usb/$dev,$product,$model,$name"
+	done
+}
