a35d32b6d9dede01388740691c67053aee3cbfd2
Chen Minqiang <ptpt52@gmail.com>
2023-11-28 17:45:38 +0800

    openvpn: change tap mtu 1404

--- natcap/files/natcapd.openvpn.sh
+++ natcap/files/natcapd.openvpn.sh
@@ -22,7 +22,7 @@ make_config()
 
 	if [ "$mode" = "1" ]; then
 		cat ${BASE_CONFIG} | sed "s/^remote .*4911$/remote $hname.dns.x-wrt.com 4911/;s/^proto tcp$/proto $PROTO/;s/dev tun/dev tap/"
-		echo tun_mtu 1434
+		echo tun_mtu 1404
 	else
 		cat ${BASE_CONFIG} | sed "s/^remote .*4911$/remote $hname.dns.x-wrt.com 4911/;s/^proto tcp$/proto $PROTO/"
 		echo tun_mtu 1420
@@ -100,7 +100,7 @@ make_config()
 			uci set openvpn.natcapovpn_$p.dev="natcap$p"
 			if [ "$mode" = "1" ]; then
 				uci set openvpn.natcapovpn_$p.dev_type='tap'
-				uci set openvpn.natcapovpn_$p.tun_mtu='1434'
+				uci set openvpn.natcapovpn_$p.tun_mtu='1404'
 			else
 				uci set openvpn.natcapovpn_$p.dev_type='tun'
 				uci set openvpn.natcapovpn_$p.tun_mtu='1420'
