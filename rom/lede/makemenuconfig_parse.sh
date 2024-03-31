f857401e1d217ff194f4ed021e14417ec0c52b52
Chen Minqiang <ptpt52@gmail.com>
2024-04-01 07:41:22 +0800

    config.ramips-mt7620-nosymbol: add bolt_bl100

--- rom/lede/makemenuconfig_parse.sh
+++ rom/lede/makemenuconfig_parse.sh
@@ -752,6 +752,7 @@ for t in $targets; do
 		TARGET_DEVICE_ramips_mt7621_DEVICE_mercusys_mr70x-v1|\
 		TARGET_DEVICE_ath79_nand_DEVICE_glinet_gl-e750|\
 		TARGET_DEVICE_ramips_mt7620_DEVICE_bolt_bl201|\
+		TARGET_DEVICE_ramips_mt7620_DEVICE_bolt_bl100|\
 		TARGET_DEVICE_ath79_nand_DEVICE_glinet_gl-x1200-nor|\
 		TARGET_DEVICE_ath79_nand_DEVICE_glinet_gl-x1200-nor-nand|\
 		TARGET_DEVICE_ramips_mt76x8_DEVICE_hiwifi_hc5611|\
@@ -1491,6 +1492,7 @@ for t in $targets; do
 		TARGET_DEVICE_mediatek_filogic_DEVICE_tenbay_wr3000k-gsw-emmc-nor|\
 		TARGET_DEVICE_ramips_mt76x8_DEVICE_xiaomi_mi-ra75|\
 		TARGET_DEVICE_ramips_mt7620_DEVICE_bolt_bl201|\
+		TARGET_DEVICE_ramips_mt7620_DEVICE_bolt_bl100|\
 		TARGET_DEVICE_mediatek_filogic_DEVICE_asus_tuf-ax4200|\
 		TARGET_DEVICE_mediatek_filogic_DEVICE_tenbay_ac-2210e|\
 		TARGET_DEVICE_mediatek_filogic_DEVICE_tenbay_ac-2205ex|\
