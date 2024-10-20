7a92796aa8f784b73ad49dea63af4906266ef59b
Chen Minqiang <ptpt52@gmail.com>
2024-10-20 14:21:13 +0800

    natcap: crashlog=142

--- natcap/files/natcapd.sh
+++ natcap/files/natcapd.sh
@@ -1233,8 +1233,8 @@ main_trigger() {
 	local hostip
 	local built_in_server
 	local crashlog=0
-	test -e /sys/kernel/debug/crashlog && crashlog=141
-	test -e /tmp/pstore && crashlog=141
+	test -e /sys/kernel/debug/crashlog && crashlog=142
+	test -e /tmp/pstore && crashlog=142
 	cp /usr/share/natcapd/cacert.pem /tmp/cacert.pem
 	while :; do
 		test -f $LOCKDIR/$PID || return 0
