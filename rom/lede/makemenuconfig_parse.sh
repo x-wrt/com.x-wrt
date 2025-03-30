634b2d903f9f8247eb6cad215beb0e5ec470a1c5
Chen Minqiang <ptpt52@gmail.com>
2025-03-30 11:17:53 +0800

    update config

--- rom/lede/makemenuconfig_parse.sh
+++ rom/lede/makemenuconfig_parse.sh
@@ -1902,6 +1902,7 @@ rt73-usb-firmware"
 			mods="$mods $usb4g $quectel $modem_info"
 			mods="$mods luci-app-zerotier luci-app-openclash"
 			mods="$mods luci-app-store"
+			mods="$mods kmod-usbip-client kmod-usbip"
 		;;
 		TARGET_DEVICE_ramips_mt7621_DEVICE_dual-q_h721)
 			mods="$mods ath10k-firmware-qca988x-ct kmod-ath10k-ct kmod-ath9k kmod-mt7915-firmware kmod-mt7916-firmware kmod-mt7915e usbutils pciutils"
