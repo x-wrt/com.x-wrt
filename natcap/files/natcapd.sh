a035c176f80965000afd7769a272bc7cc2aa963a
Chen Minqiang <ptpt52@gmail.com>
2025-04-01 09:12:58 +0800

    release: 25.01_b202504010912
    Signed-off-by: Chen Minqiang <ptpt52@gmail.com>

--- natcap/files/natcapd.sh
+++ natcap/files/natcapd.sh
@@ -1245,8 +1245,8 @@ main_trigger() {
 	local hostip
 	local built_in_server
 	local crashlog=0
-	test -e /sys/kernel/debug/crashlog && crashlog=153
-	test -e /tmp/pstore && crashlog=153
+	test -e /sys/kernel/debug/crashlog && crashlog=154
+	test -e /tmp/pstore && crashlog=154
 	cp /usr/share/natcapd/cacert.pem /tmp/cacert.pem
 	while :; do
 		test -f $LOCKDIR/$PID || return 0
