a8e0666963f7f9a68eb3fb3768a8804d97d2d6ef
brudalevante <continentalwl@gmail.com>
2025-07-10 18:54:45 +0200

    Primer commit: estructura completa de apps y parches

--- x-wrt-logo/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-inkscape -z -w 1024 x-wrt-logo-Aldrich-out.svg -e x-wrt-logo-Aldrich-out.png 
-inkscape -z -w 1024 x-wrt-logo-Aldrich-raw.svg -e x-wrt-logo-Aldrich-raw.png
-inkscape -z -w 1024 x-wrt-logo-Aldrich-raw-cicle.svg -e x-wrt-logo-Aldrich-raw-cicle.png 
-inkscape -z -w 1024 x-wrt-logo-Aldrich-raw-rect.svg -e x-wrt-logo-Aldrich-raw-rect.png 
