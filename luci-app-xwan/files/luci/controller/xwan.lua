6f1af8f71c416ab3afb05ebd1d95e76e1991025d
Minqiang Chen <ptpt52@email.com>
2026-06-19 04:50:40 +0800

    luci-app-xwan: update trans

--- luci-app-xwan/files/luci/controller/xwan.lua
+++ luci-app-xwan/files/luci/controller/xwan.lua
@@ -5,7 +5,7 @@ module("luci.controller.xwan", package.seeall)
 function index()
 	local page
 
-	page = entry({"admin", "network", "xwan"}, cbi("xwan/xwan"), _("Xwan"))
+	page = entry({"admin", "network", "xwan"}, cbi("xwan/xwan"), _("Multi-WAN Dialing"))
 	page.leaf = true
 	page.acl_depends = { "luci-app-xwan" }
 end
--- luci-app-xwan/files/luci/model/cbi/xwan/xwan.lua
+++ luci-app-xwan/files/luci/model/cbi/xwan/xwan.lua
@@ -1,29 +1,29 @@
 -- Copyright 2019 X-WRT <dev@x-wrt.com>
 
-m = Map("xwan", translate("Xwan"))
+m = Map("xwan", translate("Multi-WAN Dialing"))
 
-s = m:section(TypedSection, "xwan", translate("Xwan Settings"))
+s = m:section(TypedSection, "xwan", translate("Multi-WAN Dialing Settings"))
 s.addremove = false
 s.anonymous = true
 
-e = s:option(Flag, "enabled", translate("Enable xwan"), translate("multiwan on single interface"))
+e = s:option(Flag, "enabled", translate("Enable multi-WAN dialing"), translate("Create multiple WAN connections on a single physical interface."))
 e.default = e.disabled
 e.rmempty = false
 
-e = s:option(Value, "number", translate("Number of xwan"))
+e = s:option(Value, "number", translate("Number of WAN instances"))
 e.datatype = "and(uinteger,min(1),max(60))"
 e.rmempty  = false
 
-e = s:option(Flag, "balanced", translate("Auto balanced setup"))
+e = s:option(Flag, "balanced", translate("Configure load balancing automatically"))
 e.default = e.disabled
 e.rmempty = false
 
-e = s:option(DynamicList, "track_ip", translate("Tracking hostname or IP address"), translate("This hostname or IP address will be pinged to determine if the link is up or down. Leave blank to assume interface is always online"))
+e = s:option(DynamicList, "track_ip", translate("Health check hostnames or IP addresses"), translate("Ping these hosts to determine whether each WAN link is up. Leave empty to treat the links as always online."))
 e.datatype = 'host'
 e.default = ""
 e.placeholder = "gateway"
 
-e = s:option(ListValue, 'family', translate("Internet Protocol"))
+e = s:option(ListValue, 'family', translate("IP family"))
 e.default = ''
 e:value('', translate('IPv4 and IPv6'))
 e:value('ipv4', translate('IPv4'))
